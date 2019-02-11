841c3dd2fde939410959a342dc954a81c5282498
Yuqi <arjun.sardjoe.missier@live.nl>
2019-02-11 18:46:28 +0100

    updated routing

--- practicum_1/1/server.js
+++ practicum_1/1/server.js
@@ -8,13 +8,29 @@ const PORT = 8080;
 
 // Routes
 server = http.createServer();
-fs.readFile('./index.html', 'utf-8', (err, html) => {
-    server.on('request', (req, res) => {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        var page = html.replace('*|var|*', url.parse(req.url, true).query.name);
-        res.end(page);
-    });
+server.on('request', (req, res) => {
+    var route = url.parse(req.url, true);
+    console.log(req.url, route);
+    switch(route.pathname) {
+        case '/':
+            home(req, res, route.query.name);
+            break;
+        default:
+            home(req, res, route.query.name);
+            break;
+    }
 });
+
+function home(req, res, name) {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    fs.readFile('./index.html', 'utf-8', (err, html) => {
+        if(err) {
+            res.end(err);
+        }
+        html = html.replace('*|var|*', name);
+        res.end(html);
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Node server created on port ${PORT}`);
