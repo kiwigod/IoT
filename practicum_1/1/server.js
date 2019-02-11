d6b4f807c037ac4e38eb7b210544b90f7f8482b9
Yuqi <arjun.sardjoe.missier@live.nl>
2019-02-11 22:22:27 +0100

    finished 1

--- practicum_1/1/server.js
+++ practicum_1/1/server.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 // Constants
 const PORT = 8080;
 
+// Datasets
+var cars = require('./cars.json');
+
 // Routes
 server = http.createServer();
 server.on('request', (req, res) => {
@@ -14,6 +17,10 @@ server.on('request', (req, res) => {
     switch(route.pathname) {
         case '/':
             home(req, res, route.query.name);
+            break;
+        case '/cars':
+            if (!isEmpty(route.query)) applyFilterOnDataset(route.query, cars, res);
+            else res.end(JSON.stringify(cars));
             break;
         default:
             home(req, res, route.query.name);
@@ -30,6 +37,22 @@ function home(req, res, name) {
         html = html.replace('*|var|*', name);
         res.end(html);
     });
+}
+
+function applyFilterOnDataset(filters, data, res) {
+    let json_res = [];
+    for (var prop in filters) {
+        for (var i in data) {
+            if (data[i]['engine'] === filters[prop])
+                json_res.push(data[i]);
+        }
+    }
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    res.end(JSON.stringify(json_res));
+}
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
 }
 
 server.listen(PORT, () => {
