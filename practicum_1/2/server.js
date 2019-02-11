916e377d8452305add96fa370b07889447a92f6c
Yuqi <arjun.sardjoe.missier@live.nl>
2019-02-11 23:43:25 +0100

    finshed 2d

--- practicum_1/2/server.js
+++ practicum_1/2/server.js
@@ -4,13 +4,20 @@ const mqtt = require('mqtt');
 // Constants
 const PORT = 8080;
 const client = mqtt.connect('mqtt://broker.mqttdashboard.com');
+const client2 = mqtt.connect('mqtt://broker.mqttdashboard.com');
 
 client.on('connect', () => {
-    client.subscribe('number');
+//    client.subscribe('number');
+    client.subscribe('weather');
+    client2.subscribe('weather');
 });
 
 client.on('message', (topic, message) => {
     console.log("Client received the following message:\n" + message.toString());
+});
+
+client2.on('message', (topic, message) => {
+    console.log("Client 2 received the following message:\n" + message.toString());
 });
 
 function publishRandomNumber() {
@@ -18,7 +25,15 @@ function publishRandomNumber() {
     client.publish('number', Math.random().toString());
 }
 
+function publishWeather() {
+    var weather = {
+        'city' : 'Deflt',
+        'temp' : Math.floor((Math.random() * 30) + 1)
+    };
+    client.publish('weather', JSON.stringify(weather));
+}
+
 setInterval(() => {
-    publishRandomNumber();
+    publishWeather();
 }, 3000);
 
